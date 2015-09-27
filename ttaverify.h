9c27da4012c9fa3ec76081ef1696b1e058525b3f
Hakkin Lain <Hakkin@github>
2015-09-27 16:21:02 -0700

    Adds ttaverify.h.

--- ttaverify.h
+++ ttaverify.h
@@ -1,6 +1,16 @@
 #ifndef TTAVERIFY_H_INCLUDED
 #define TTAVERIFY_H_INCLUDED
 
+typedef enum
+{
+    TTA_OK,        // Success
+    TTA_BADIO,     // IO error (couldn't read file)
+    TTA_BADFORMAT, // Format error (not TTA file)
+    TTA_BADHEADER, // Stream header CRC error
+    TTA_BADSEEK,   // Seek table CRC error
+    TTA_BADFRAME   // Frame CRC error
+} ttaStatus;
 
+ttaStatus ttaVerify(char *filename);
 
 #endif // TTAVERIFY_H_INCLUDED

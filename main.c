3dbf789acb98f67337e051bc3d0319b16844ef81
Hakkin Lain <Hakkin@github>
2015-09-27 19:51:02 -0700

    Changed error checking, program should be functional now.

--- main.c
+++ main.c
@@ -11,32 +11,32 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    ttaStatus status;
-    if ((status = ttaVerify(argv[1])) != TTA_OK)
+    ttaStatus status = ttaVerify(argv[1]);
+    printf("%s: ", argv[1]);
+    switch (status)
     {
-        switch (status)
-        {
-        case TTA_BADIO:
-            printf("Error: Could not read file '%s'\n", argv[1]);
-            return TTA_BADIO;
-        case TTA_BADMEM:
-            printf("Error: Could not allocate memory for file '%s'\n", argv[1]);
-            return TTA_BADMEM;
-        case TTA_BADFORMAT:
-            printf("Error: File '%s' is not a TTA file\n", argv[1]);
-            return TTA_BADFORMAT;
-        case TTA_BADHEADER:
-            printf("Error: File '%s' has a corrupt header\n", argv[1]);
-            return TTA_BADHEADER;
-        case TTA_BADSEEK:
-            printf("Error: File '%s' has a corrupt seek table\n", argv[1]);
-            return TTA_BADSEEK;
-        case TTA_BADFRAME:
-            printf("Error: File '%s' has one or more corrupt frames\n", argv[1]);
-        case TTA_OK: // To stop compiler warning
+        case TTA_OK:
+            printf("OK\n");
             break;
-        }
+        case TTA_BADIO:
+            printf("Could not read file\n");
+            break;
+        case TTA_BADMEM:
+            printf("Could not allocate memory for file\n");
+            break;
+        case TTA_BADFORMAT:
+            printf("File is not a TTA file\n");
+            break;
+        case TTA_BADHEADER:
+            printf("File has a corrupt header\n");
+            break;
+        case TTA_BADSEEK:
+            printf("File has a corrupt seek table\n");
+            break;
+        case TTA_BADFRAME:
+            printf("File has one or more corrupt frames\n");
+            break;
     }
 
-    return 0;
+    return status;
 }
